f4a896c1b209d79882cff4e3f3e763ca97e09cd1
Ande Praveen <praveenande2023@gmail.com>
2024-10-15 20:48:43 +0530

    Clean Code Testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,64 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import pluginReact from 'eslint-plugin-react';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser, // Specify the parser for TypeScript files
+      globals: globals.browser, // Define global variables
     },
+    settings: {
+      react: {
+        version: 'detect', // Detect React version
+      },
+    },
+
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      prettier, // Use Prettier as a plugin object
+      '@typescript-eslint': tseslint, // Add TypeScript ESLint as a plugin object
+      react: pluginReact, // Add React plugin,
+      import: importPlugin,
     },
+    rules: {},
+  },
+
+  {
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      // Additional rules from TypeScript ESLint
+      ...tseslint.configs.recommended.rules,
     },
   },
-)
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    /* -----> Override the Rules <----- */
+    // Write your own rules
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+          allowSeparatedGroups: true,
+        },
+      ], // Sorting imports order
+      'no-unused-vars': 'error', // Unused variables
+      indent: ['error', 2], // Indentation
+      quotes: ['error', 'single'], // Strings must be in single quotes
+      'no-console': 'warn', // Allow console statements but warn
+      'no-debugger': 'error', // Throw an error if the debugger statement is used
+      eqeqeq: ['error', 'always'], // Use === and !== instead of == and !=
+      semi: ['error', 'always'], // Use semicolons at the end of statements
+      curly: ['error', 'all'], // Require curly braces for all control statements
+      camelcase: 'error', // Use camelCase for variable and function names
+      'no-trailing-spaces': 'error', // Disallow trailing whitespace at the end of lines
+    },
+  },
+];
